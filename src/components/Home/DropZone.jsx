07b2f2a003d866abea58b779e64ca6444a194972
Saif Rahman <saif.rahman@udplatforms.com>
2022-05-01 16:14:45 +0600

    upload added

--- src/components/Home/DropZone.jsx
+++ src/components/Home/DropZone.jsx
@@ -1,16 +1,42 @@
+import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import axios from 'axios'
 import { DropzoneArea } from 'material-ui-dropzone'
 import { useState } from 'react'
+import { CircularProgress } from '@mui/material'
 
 const DropZone = () => {
   const [file, setFile] = useState(null)
+  const [result, setResult] = useState([])
+  const [loading, setLoading] = useState(false)
 
   const handleChange = (files) => {
     if (files[0]) {
       setFile(files[0])
       console.log(files[0])
     }
+  }
+
+  const onSubmit = (file) => {
+    setLoading(true)
+    var formData = new FormData()
+    formData.append('file', file)
+    axios({
+      method: 'POST',
+      url: 'http://localhost:5000/predict',
+      data: formData,
+      headers: { 'Content-Type': 'multipart/form-data' },
+    })
+      .then((res) => {
+        setLoading(false)
+        console.log(res)
+        setResult(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   console.log(file)
@@ -24,7 +50,7 @@ const DropZone = () => {
         clearOnUnmount={true}
       />
       <Button
-        onClick={() => console.log(file)}
+        onClick={() => onSubmit(file)}
         sx={{
           marginTop: '20px !important',
           padding: '6px !important',
@@ -39,6 +65,39 @@ const DropZone = () => {
       >
         Check
       </Button>
+      {loading && (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      )}
+      {result !== [] && loading === false && (
+        <Paper sx={{ mt: 3, p: 1 }}>
+          <Typography
+            gutterBottom
+            variant='h4'
+            fontWeight='bold'
+            align='center'
+          >
+            Result
+          </Typography>
+          <Typography gutterBottom variant='h5' fontWeight='bold'>
+            Boxes:
+          </Typography>
+          {result?.boxes?.map((e) => (
+            <Typography>{e?.toString()}</Typography>
+          ))}
+          <Typography gutterBottom variant='h5' fontWeight='bold'>
+            Labels:
+          </Typography>
+          {<Typography>{result?.labels?.toString()}</Typography>}
+        </Paper>
+      )}
     </Box>
   )
 }
